ecc72d6b15b03dc5976205c3546b8db040c19fd3
luis <luisbigvargas@gmail.com>
2015-05-30 15:57:05 -0400

    add T07_1

---
+++ T07_1_MVC_person_form/src/main/java/com/lvargas/mvc1_person_form/models/Person.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lvargas.mvc1_person_form.models;
+
+/**
+ *
+ * @author luis
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
